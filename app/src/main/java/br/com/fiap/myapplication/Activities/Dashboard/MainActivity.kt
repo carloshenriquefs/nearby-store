1e6531f492038cbe49a77ee674b1f022ceab663a
Carlos Henrique <ichfs9108@gmail.com>
2025-04-06 04:09:08 -0300

    feat: add top bar

--- app/src/main/java/br/com/fiap/myapplication/Activities/Dashboard/MainActivity.kt
+++ app/src/main/java/br/com/fiap/myapplication/Activities/Dashboard/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.fiap.myapplication.R
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +29,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 @Preview
 fun DashboardScreen() {
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setStatusBarColor(color = colorResource(id = R.color.blue))
     Scaffold(
         bottomBar = { MyBottomBar() }
     ) { paddingValues ->
@@ -36,6 +39,8 @@ fun DashboardScreen() {
                 .fillMaxSize()
                 .background(color = colorResource(R.color.lightBlue))
                 .padding(paddingValues = paddingValues)
-        ) { }
+        ) {
+            item { TopBar() }
+        }
     }
 }
